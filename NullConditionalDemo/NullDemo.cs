d94efe1346280006f448f47c9ed08fdf584c1390
Josh Eastburn <josh@falafel.com>
2015-09-24 08:55:15 -0400

    Adding demo solution

---
+++ NullConditionalDemo/NullDemo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullConditionalDemo
+{
+    public class NullDemo
+    {
+        public event EventHandler DataChanged;
+
+        public void HandleEventWithError()
+        {
+            DataChanged(this, new EventArgs());
+        }
+
+        public void HandleEventWithNullCheck()
+        {
+            if (DataChanged != null)
+            {
+                DataChanged(this, new EventArgs());
+            }
+        }
+
+        public void HandleEvent()
+        {
+            DataChanged?.Invoke(this, new EventArgs());
+        }
+
+        public string GetNameFromItem(NullDemoData item)
+        {
+            return item?.Name;
+        }
+        public int GetIdFromItem(NullDemoData item)
+        {
+            return item?.Id ?? 0;
+        }
+
+        public string GetFirstNameFromList(IList<NullDemoData> list = null)
+        {
+            return list?[0]?.Name ?? "No Name";
+        }
+    }
+
+    public class NullDemoData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ NullConditionalDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NullConditionalDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NullConditionalDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c0bf33a8-0a9f-4b82-bf09-77e0512d6627")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("NullConditionalDemo.Tests")]
+[assembly: InternalsVisibleTo("NullConditionalDemo.Explorables")]
+
